e631406b8e398f9e37711bc866e45e6b22b1f675
blghnks <bilgehankuch@gmail.com>
2025-11-02 15:12:48 +0300

    week6 2

---
+++ week6/app/src/main/java/msku/ceng/madlab/week6/Movie.java
@@ -0,0 +1,100 @@
+package msku.ceng.madlab.week6;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie implements Parcelable, Serializable {
+    private String name;
+    private String director;
+    private int year;
+    private List<String> stars = new ArrayList<>();
+    private String description;
+
+    public Movie(String name, String director, int year, List<String> stars, String description) {
+        this.name = name;
+        this.director = director;
+        this.year = year;
+        this.stars = stars;
+        this.description = description;
+    }
+
+    protected Movie(Parcel in) {
+        name = in.readString();
+        director = in.readString();
+        year = in.readInt();
+        stars = in.createStringArrayList();
+        description = in.readString();
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getStars() {
+        return stars;
+    }
+
+    public void setStars(List<String> stars) {
+        this.stars = stars;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(director);
+        parcel.writeInt(year);
+        parcel.writeStringList(stars);
+        parcel.writeString(description);
+    }
+}
